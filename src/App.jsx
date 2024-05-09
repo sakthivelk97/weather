1b8cb02f7fd1c1e09a46795deb90cced4067eacf
sakthivelk97 <sakthivelmaddy97@gmail.com>
2024-05-09 11:12:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Weather from './components/weather'
+
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className="app">
+    
+      <Weather/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/search/index.jsx
@@ -0,0 +1,19 @@
+
+
+
+export default function Search({search, setSearch, handleSearch}){
+    return (
+    <div className="search-engine">
+        <input
+         type="text" 
+         className="city-search"
+         placeholder="Enter city name"
+         name="search"
+         value={search}
+         onChange={(event)=> setSearch(event.target.value)}
+         />
+         <button className="search-btn" onClick={handleSearch}>
+            Search
+        </button>
+    </div>
+)}
